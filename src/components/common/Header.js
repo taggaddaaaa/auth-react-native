91d4f4bb4331c05be366591ed3df0fa276838046
taggaddaaaa <scaizerg@student.42.fr>
2017-09-14 22:51:44 +0200

    import components that i have created inside my precedent app in react-native 'album'

---
+++ src/components/common/Header.js
@@ -0,0 +1,38 @@
+// Import libraries for making a component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+// Make a component
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return  (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F3C7A1',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#E87975',
+    shadowOffset: { width: 0, height: 2},
+    shadowOpacity: 0.9,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    color: '#FFFFFF',
+    fontSize: 20
+  }
+};
+
+
+// Make the component available to other parts of the app
+export default Header;
