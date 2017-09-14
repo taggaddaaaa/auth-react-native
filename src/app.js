49085bbd6eb0829fadf1f5cf0eaa3bff3dc2c0c8
taggaddaaaa <scaizerg@student.42.fr>
2017-09-14 22:50:18 +0200

    Create new auth app in react-native make a file app.js because index.android/ios.js are the same

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>This is my new app!</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
