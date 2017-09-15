8d04616cc675b25a42bc4e297ac52e4d33e2fac3
taggaddaaaa <scaizerg@student.42.fr>
2017-09-15 14:52:01 +0200

    Set up Firebase in the app - create a firebase app in https://console.firebase.google.com - enable the auth by email in firebase console - install firebase library in dependencies with npm i --save firebase in the app - import firebase BEFORE import of components in app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyA7Qi3wuiPQtzFZaY8FUHAKFU4AzV8VIHI',
+      authDomain: 'auth-react-native-f5ffe.firebaseapp.com',
+      databaseURL: 'https://auth-react-native-f5ffe.firebaseio.com',
+      projectId: 'auth-react-native-f5ffe',
+      storageBucket: 'auth-react-native-f5ffe.appspot.com',
+      messagingSenderId: '318563548875'
+    });
+
+  }
+
+
   render() {
     return (
       <View>
