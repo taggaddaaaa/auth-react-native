1ae2c0d5ed0583eaec30e8b11676132515965aa6
taggaddaaaa <scaizerg@student.42.fr>
2017-09-24 17:49:18 +0200

    init a state loggedIn to null (and not to false to switch between three state conditions and render a spinner in case I am not loggedIn or loggedOut yet. Add signOut function of firebase to the log out button

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm'
 
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyA7Qi3wuiPQtzFZaY8FUHAKFU4AzV8VIHI',
@@ -16,6 +18,28 @@ class App extends Component {
       messagingSenderId: '318563548875'
     });
 
+    firebase.auth().onAuthStateChanged((user) => {
+       if (user) {
+         this.setState({ loggedIn: true });
+       } else {
+         this.setState({ loggedIn: false });
+       }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm/>;
+      default:
+        return <Spinner size="large" />
+    }
   }
 
 
@@ -23,7 +47,7 @@ class App extends Component {
     return (
       <View>
         <Header headerText="Authentification"/>
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
